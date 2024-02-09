1f827c22cb6910fce6a54d8023fcac4c03d86398
Kaviyarasan <kaviyarasan110920@gmail.com>
2024-02-09 17:42:29 +0530

    -

---
+++ src/Data/ProjectData.js
@@ -0,0 +1,24 @@
+import TodoList from '../assets/to-do-list.png'
+import Pro2 from '../assets/portfolio.png'
+import Calculator from '../assets/Calculator.PNG'
+
+export const ProjectData = [
+    {
+        PName : 'Todo List',
+        Demo : 'https://kaviyarasan600.github.io/Todo-List/',
+        Score: 'https://github.com/Kaviyarasan600/Todo-List',
+        img : `${TodoList}`,
+    },
+    {
+        PName : 'Portfolio',
+        Demo : 'https://kaviyarasan600.github.io/Portfolio/',
+        Score: 'https://github.com/Kaviyarasan600/Todo-List',
+        img : `${Pro2}`,
+    },
+    {
+        PName : 'Calculator',
+        Demo : 'https://kaviyarasan600.github.io/Calculator/',
+        Score: 'https://github.com/Kaviyarasan600/Calculator.git',
+        img : `${Calculator}`,
+    },
+]
