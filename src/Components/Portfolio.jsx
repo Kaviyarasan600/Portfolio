b3f31b94438ad26d34025a682efdb897422893c9
Kaviyarasan <kaviyarasan110920@gmail.com>
2024-02-09 18:52:16 +0530

    -

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -3,7 +3,7 @@ import { Skills } from "./Skills/Skills"
 import { Home } from "./Home/Home"
 import { Project } from "./Project/Project"
 import { Footer } from "./Footer/Footer"
-import { NavBar } from "./Navbar/Navbar"
+import { NavBar } from "./NavBar/NavBar"
 
 
 
